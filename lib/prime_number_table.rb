34bf9261be3d4babf29c11b571be53e6362eec56
Evgeny Peleh <pelehev@gmail.com>
2020-10-25 16:53:53 +0300

    Implement prime methods

--- lib/prime_number_table.rb
+++ lib/prime_number_table.rb
@@ -4,5 +4,28 @@ require 'prime_number_table/version'
 
 module PrimeNumberTable
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.print(_table_size)
+    raise 'TODO'
+  end
+
+  def self.prime_number(index)
+    number = 0
+    (index -= 1 if prime?(number += 1)) while index.positive?
+    number
+  end
+
+  def self.prime?(number)
+    return false if number < 2
+
+    @prime_numbers ||= [false, false]
+
+    if @prime_numbers[number].nil?
+      (@prime_numbers.size..number).each do |n|
+        @prime_numbers[n] = (2..n / 2).none? { |i| (n % i).zero? }
+      end
+    end
+
+    @prime_numbers[number]
+  end
 end
--- spec/prime_number_table_spec.rb
+++ spec/prime_number_table_spec.rb
@@ -1,11 +1,34 @@
 # frozen_string_literal: true
 
+PRIME_NUMBERS = [
+  2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103,
+  107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223,
+  227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347,
+  349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463,
+  467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607,
+  613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743,
+  751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883,
+  887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997
+].freeze
+
 RSpec.describe PrimeNumberTable do
   it 'has a version number' do
     expect(PrimeNumberTable::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#prime_number' do
+    subject { described_class.method(:prime_number) }
+
+    it 'returns a prime number by its index' do
+      PRIME_NUMBERS.each_with_index { |n, i| expect(subject[i + 1]).to eq(n) }
+    end
+  end
+
+  describe '#prime?' do
+    subject { described_class.method(:prime?) }
+
+    it 'determines if the number is prime' do
+      (0..1_000).each { |n| expect(subject[n]).to eq(PRIME_NUMBERS.include?(n)) }
+    end
   end
 end
