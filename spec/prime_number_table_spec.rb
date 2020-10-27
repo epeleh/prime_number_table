b61c033965590a274ea57a23bc8ec2a23bd226a8
Evgeny Peleh <pelehev@gmail.com>
2020-10-27 15:20:14 +0300

    Update tests

--- spec/prime_number_table_spec.rb
+++ spec/prime_number_table_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FIBONACCI_NUMBERS = [
-  1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10_946, 17_711,
+  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10_946, 17_711,
   28_657, 46_368, 75_025, 121_393, 196_418, 317_811, 514_229, 832_040, 1_346_269, 2_178_309, 3_524_578,
   5_702_887, 9_227_465, 14_930_352, 24_157_817, 39_088_169, 63_245_986, 102_334_155
 ].freeze
@@ -102,7 +102,7 @@ RSpec.describe PrimeNumberTable do
     subject { described_class.method(:fibonacci_number) }
 
     it 'returns a fibonacci number by its index' do
-      FIBONACCI_NUMBERS.each_with_index { |n, i| expect(subject.call(i + 1)).to eq(n) }
+      FIBONACCI_NUMBERS.each_with_index { |n, i| expect(subject.call(i)).to eq(n) }
     end
   end
 
